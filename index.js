03b2f0fd82dbe7599135828a0b4550e80c2b2673
Jose Moreno <morenojr90@yahoo.com>
2018-12-21 12:26:49 -0800

    get url and parse it, trim url path

--- index.js
+++ index.js
@@ -3,10 +3,25 @@
 
 //Dependencies
 var http = require('http');
+var url = require('url');
 
+//  This createServer function is run everytime someone requests our App.
 // The server should respond to all request with a string
 var server = http.createServer(function(req,res) {
+
+  // Get URL and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  //this regex trims off slashes from path
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send the response
   res.end("Hello World\n");
+
+  // Log the request path
+  console.log('Request received on path: ' + trimmedPath);
 })
 
 
