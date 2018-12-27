cdc4dc3390d07d9c9907b48b928d97ab667854da
Jose Moreno <morenojr90@yahoo.com>
2018-12-26 16:38:16 -0800

    create cert and key, create unified server function, both http and https ports now listening, used fs to read key and cert for https

--- index.js
+++ index.js
@@ -2,16 +2,40 @@
 
 
 
-
 //Dependencies
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config')
+var fs = require('fs');
 
-//  This createServer function is run everytime someone requests our App. The server should respond to all request with a string
-var server = http.createServer(function(req,res) {
+//  Instantiate the HTTP server
+var httpServer = http.createServer(function(req,res) {
+  unifiedServer(req, res);
+})
 
+//Start the server
+httpServer.listen(config.httpPort, function() {
+  console.log("The server is listening on port " + config.httpPort  );
+})
+
+
+//  Instantiate the HTTPS createServer
+var httpsServerOptions = {
+  'key' : fs.readFileSync('./https/key.pem'),
+  'cert' : fs.readFileSync('./https/cert.pem')
+}
+var httpsServer = https.createServer(httpsServerOptions, function(req,res) {
+  unifiedServer(req,res);
+})
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function() {
+  console.log("The server is listening on port " + config.httpsPort);
+})
+// All the server logic for both the http and https server
+var unifiedServer = function(req,res) {
   // Get URL and parse it
   var parsedUrl = url.parse(req.url, true);
 
@@ -67,15 +91,11 @@ var server = http.createServer(function(req,res) {
       res.end(payloadString);
 
     // Log the request path
-      console.log('Return this response: ' , statusCode,payloadString);
+      console.log('Returning this response: ' , statusCode,payloadString);
     });
-  })
-})
+  });
+}
 
-//Start the server, and have it listen on port 3000
-server.listen(config.port, function() {
-  console.log("The server is listening on port " + config.port + " in " + config.envName+ " mode");
-})
 // Define the handlers
 var handlers = {};
 // Sample hanlder
